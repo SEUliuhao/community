f90fdc395396cf4b10c97012fc7b8c9b40136fa2
SEUliuhao <1131592949@qq.com>
2020-02-09 19:45:07 +0800

    add sql ddl

--- README.md
+++ README.md
@@ -2,10 +2,29 @@
 
 ## 资料
 [Spring文档](https://spring.io/guides)
+
 [Spring Web文档](https://spring.io/guides/gs/serving-web-content/)
+
 [es](https://elasticsearch.cn/)
+
 [Bootstrap](https://v3.bootcss.com/)
+
 [oAuth](https://developer.github.com/apps/building-github-apps/creating-a-github-app/)
 
+[springboot mybatis](http://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/)
+
+[spring](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
 ## 工具
 https://git-scm.com/
+##脚本
+```sql
+CREATE CACHED TABLE "PUBLIC"."USER"(
+    "ID" int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    "ACCOUNT_ID" VARCHAR(100),
+    "NAME" VARCHAR(50),
+    "TOKEN" CHAR(36),
+    "GMT_CREATE" BIGINT,
+    "GMT_MODIFIED" BIGINT
+)
+
+```
--- src/main/java/life/liuhao/community/community/Controller/AuthorizeController.java
+++ src/main/java/life/liuhao/community/community/Controller/AuthorizeController.java
@@ -1,14 +1,21 @@
 package life.liuhao.community.community.Controller;
 
+import Model.User;
 import life.liuhao.community.community.DTO.AccessTokenDto;
 import life.liuhao.community.community.DTO.GithubUser;
+import life.liuhao.community.community.Mapper.UserMapper;
 import life.liuhao.community.community.Provider.GithubProvider;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -21,11 +28,15 @@ public class AuthorizeController {
     @Value(("${github.redirect.url}"))
     private  String redirectUrl;
 
+    @Autowired
+    private UserMapper userMapper;
+
 
 
    @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                            @RequestParam(name ="state") String state){
+                           @RequestParam(name ="state") String state,
+                           HttpServletRequest request){
        AccessTokenDto accessTokenDto = new AccessTokenDto();
        accessTokenDto.setClient_id(clientId);
        accessTokenDto.setClient_secret(clientSecret);
@@ -33,9 +44,23 @@ public class AuthorizeController {
        accessTokenDto.setRedirect_url(redirectUrl);
        accessTokenDto.setState(state);
        String accessToken = githubProvider.getAccessToken(accessTokenDto);
-       GithubUser user = githubProvider.getUser(accessToken);
-       System.out.println(user.getName());
+       GithubUser githubUser = githubProvider.getUser(accessToken);
+       System.out.println(githubUser.getName());
+       if(githubUser!=null){
+           //登录成功，写cookie和session
+           User user = new User();
+           user.setToken(UUID.randomUUID().toString());
+           user.setName(githubUser.getName());;
+           user.setAccount_id(String.valueOf(githubUser.getId()));
+           user.setGmt_create(System.currentTimeMillis());
+           user.setGmt_modified(user.getGmt_create());
+           userMapper.insert(user);
+           request.getSession().setAttribute("user",githubUser);
+           return  "redirect:/";
+       }else {
+           //登陆失败
+           return  "redirect:/";
 
-       return "index";
+       }
     }
 }
