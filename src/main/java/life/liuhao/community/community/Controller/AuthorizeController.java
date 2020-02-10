cbb533780cbc4b053c4dd3f1a71f6cc3b4acd090
SEUliuhao <1131592949@qq.com>
2020-02-10 18:47:42 +0800

    add flyway migration

--- src/main/java/life/liuhao/community/community/Controller/AuthorizeController.java
+++ src/main/java/life/liuhao/community/community/Controller/AuthorizeController.java
@@ -14,7 +14,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -36,7 +39,7 @@ public class AuthorizeController {
    @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name ="state") String state,
-                           HttpServletRequest request){
+                           HttpServletResponse response){
        AccessTokenDto accessTokenDto = new AccessTokenDto();
        accessTokenDto.setClient_id(clientId);
        accessTokenDto.setClient_secret(clientSecret);
@@ -45,17 +48,18 @@ public class AuthorizeController {
        accessTokenDto.setState(state);
        String accessToken = githubProvider.getAccessToken(accessTokenDto);
        GithubUser githubUser = githubProvider.getUser(accessToken);
-       System.out.println(githubUser.getName());
        if(githubUser!=null){
            //登录成功，写cookie和session
            User user = new User();
-           user.setToken(UUID.randomUUID().toString());
+           String token = UUID.randomUUID().toString();
+
+           user.setToken(token);
            user.setName(githubUser.getName());;
            user.setAccount_id(String.valueOf(githubUser.getId()));
            user.setGmt_create(System.currentTimeMillis());
            user.setGmt_modified(user.getGmt_create());
            userMapper.insert(user);
-           request.getSession().setAttribute("user",githubUser);
+           response.addCookie(new Cookie("token",token));
            return  "redirect:/";
        }else {
            //登陆失败
--- src/main/java/life/liuhao/community/community/Mapper/UserMapper.java
+++ src/main/java/life/liuhao/community/community/Mapper/UserMapper.java
@@ -3,10 +3,14 @@ package life.liuhao.community.community.Mapper;
 import Model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified)  values (#{name},#{account_id},#{token},#{gmt_create},#{gmt_modified})")
-    public void insert(User user );
+    void insert(User user );
+    @Select("select * from user where token=#{token}")
+    User findMyToken(@Param("token") String token);
 }
